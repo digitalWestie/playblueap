fcb806d37111dde4b7ce24f34b4aeb98de7ac088
Rory Gianni <giannichan@gmail.com>
2014-01-18 21:16:49 +0000

    invert

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,9 +1,9 @@
 class ActionsController < ApplicationController
   def index
-    action = Action.order('created_at DESC').first
+    action = Action.order('created_at ASC').first
     if action.blank? or action.created_at < 4.minute.ago #get latest actions
       Action.get_latest_actions
     end
-    @actions = Action.order('created_at DESC').all
+    @actions = Action.order('created_at ASC').all
   end
 end
