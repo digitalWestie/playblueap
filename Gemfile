0c48472e96e69d7a14b637bad8e8ef85fa10c4af
Rory Gianni <giannichan@gmail.com>
2014-01-18 17:37:49 +0000

    cleanup crap

--- Gemfile
+++ Gemfile
@@ -21,7 +21,7 @@ group :assets do
 end
 
 gem 'jquery-rails'
-require 'twitter'
+gem 'twitter'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/action.rb
+++ app/models/action.rb
@@ -2,10 +2,6 @@ class Action < ActiveRecord::Base
   attr_accessible :body, :coefficient, :hashtag, :tweet_id, :user_id
   BLUEAP_USER_ID = 17069499 # CHANGE TO..
 
-
-r2.collection[8].attrs[:user][:id]
- => 
-
   def is_tweet_valid?(tweet)
     is_valid = tweet.attrs[:in_reply_to_user_id].eql?(BLUEAP_USER_ID) #is in reply to blueap
     is_valid = is_valid and !tweet.attrs[:user][:id].eql?(BLUEAP_USER_ID) #cant be from the app
