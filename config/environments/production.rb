274e66f824fe420c7132ed894c176a3bf6a5ed8c
Rory Gianni <giannichan@gmail.com>
2014-02-18 15:08:11 +0000

    make sure to include load pdfs in precomp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Blueap::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( load_pdfs.js, pdf.worker.js )
+  config.assets.precompile += %w( load_pdfs.js pdf.worker.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
