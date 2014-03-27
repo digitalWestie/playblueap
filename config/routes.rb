3920ca4148712dcbb294b0a8caaa0fc3dff0cd42
Rory Gianni <giannichan@gmail.com>
2014-03-27 16:10:32 +0000

    new homepage

--- config/routes.rb
+++ config/routes.rb
@@ -67,7 +67,7 @@ Blueap::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'actions#index'
+  root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
