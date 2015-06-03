ae113f7e3547167764b4f98bb651a4199fa69a85
Rory Gianni <giannichan@gmail.com>
2015-06-04 00:43:35 +0100

    context switching

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,9 +7,9 @@ class PagesController < ApplicationController
     end
 
     action = Action.accepted.order('tweet_id DESC').last
-    #if action.blank? or action.created_at < 4.minute.ago #get latest actions
-    #  Action.get_latest_actions
-    #end
+    if action.blank? or action.created_at < 4.minute.ago #get latest actions
+      Action.get_latest_actions
+    end
     @actions = Action.accepted.order('tweet_id DESC').limit(12).all
   end
 end
