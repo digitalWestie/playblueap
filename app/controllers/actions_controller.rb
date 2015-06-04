8db334e4d9b2d5b8311b281b8864e79036a715be
Rory Gianni <giannichan@gmail.com>
2015-06-04 01:47:55 +0100

    sort out seed articles

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -5,6 +5,9 @@ class ActionsController < ApplicationController
       Action.get_latest_actions
     end
     @actions = Action.accepted.order('tweet_id DESC').limit(60).all
+    @page = Refinery::Page.find_by_slug("hashtags")
+    @pages = Refinery::Page.all
+    @pages.delete_if { |p| ["Home", "hashtags", "Page not found"].include? p.title } 
   end
 
   def totals
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,18 @@ user.save
 user.roles << Role.find_by_title('Refinery')
 user.roles << Role.find_by_title('Superuser')
 user.save
+
+p = Refinery::Page.create(title: "hashtags", slug: "hashtags")
+Action::CATEGORIES.each do |hashtag, name| 
+  x = p.parts.build(title: hashtag, body: "<h2>#{name}</h2>Fusce tempor, erat eget laoreet aliquet, tellus metus eleifend lorem, id rhoncus felis est at ante. 
+    Curabitur quis est lorem. Vivamus mollis molestie auctor. In venenatis blandit dui, at fringilla ligula varius posuere. 
+    Suspendisse potenti. Sed a posuere metus, eu ornare neque. Donec tempus ac metus ornare facilisis. 
+    Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ")
+  x.save
+end
+
+x = p.parts.build(title: "all", body: "<h1>BlueAp</h1><img src='http://cdni.condenast.co.uk/646x430/a_c/bologna_cnt_9nov09_istock_b_1.jpg' style='float:left; margin:1em;'></img>Fusce tempor, erat eget laoreet aliquet, tellus metus eleifend lorem, id rhoncus felis est at ante. 
+  Curabitur quis est lorem. Vivamus mollis molestie auctor. In venenatis blandit dui, at fringilla ligula varius posuere. 
+  Suspendisse potenti. Sed a posuere metus, eu ornare neque. Donec tempus ac metus ornare facilisis. 
+  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ")
+x.save
